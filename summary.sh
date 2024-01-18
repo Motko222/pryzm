838384ce44c2be3bf255b2eb57def5c97398caa7
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-18 22:35:15 +0100

    Update summary.sh

--- summary.sh
+++ summary.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
 source ~/scripts/pryzm/config/env
-cd ~/pryzm
 
 echo   "---- SUMMARY --------------------------------------------------------------------------"
 printf "%-12s %9s %9s %9s\n" Id Balance Delegated Reward
 echo   "---------------------------------------------------------------------------------------"
 
-./pryzmd keys list | grep -E 'name|address' | sed 's/- address: //g' | sed 's/  name: //g' | paste - - | grep -v master >~/scripts/pryzm/config/keys
+pryzmd keys list | grep -E 'name|address' | sed 's/- address: //g' | sed 's/  name: //g' | paste - - | grep -v master >~/scripts/pryzm/config/keys
 
 cat ~/scripts/pryzm/config/keys | while read line
 do
    id=$(echo $line | awk '{print $2}')
    wallet=$(echo $line | awk '{print $1}')
-   balance=$(./pryzmd query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
-   valoper=$(./pryzmd keys show $WALLET --bech val | grep valoper | awk '{print $3}')
-   rewards=$(./pryzmd query distribution rewards $wallet $valoper 2>/dev/null \
+   balance=$(pryzmd query bank balances $wallet | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
+   valoper=$(pryzmd keys show $WALLET --bech val | grep valoper | awk '{print $3}')
+   rewards=$(pryzmd query distribution rewards $wallet $valoper 2>/dev/null \
      | grep amount | awk '{print $3}' | sed 's/"//g' | awk '{print $1/1000000}')
    
-   stake=$(./pryzmd query staking delegation $wallet $valoper 2>/dev/null \
+   stake=$(pryzmd query staking delegation $wallet $valoper 2>/dev/null \
      | grep amount | awk '{print $2}' | sed 's/"//g' | awk '{print $1/1000000}' )
 
    printf "%-12s %9s %9s %9s\n" \
