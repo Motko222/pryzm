a0b0f4bbbc6e09fac552d8bc39583466b643642d
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-19 21:10:01 +0100

    Create create-validator.sh

---
+++ create-validator.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+source ~/scripts/pryzm/config/env
+
+pruzmd tx checkpointing create-validator \
+--amount 20000000upryzm \
+--pubkey $(pryzmd tendermint show-validator) \
+--moniker $MONIKER \
+--identity "ID" \
+--details "DETAILS" \
+--website "WEBSITE" \
+--chain-id indigo-1 \
+--commission-rate 0.05 \
+--commission-max-rate 0.20 \
+--commission-max-change-rate 0.01 \
+--min-self-delegation 1 \
+--from $WALLET \
+--gas-adjustment 1.4 \
+--gas auto \
+--fees 10upryzm \
+-y
