d98665a1b933f8cfd354ec2310fe8398a3b01332
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-01-21 19:53:59 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -24,5 +24,5 @@ fi
 
 amount=$(( $amount * 1000000 ))upryzm
 
-babylond tx staking delegate $valoper $amount --from $key \
+pryzmd tx staking delegate $valoper $amount --from $key \
  --chain-id indigo-1 --gas-prices 0.1upryzm --gas-adjustment 1.5 --gas auto -y
