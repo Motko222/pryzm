3cb0ce86aa4cff34095c8d8eda94411ec17fb788
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-11 22:00:41 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -9,9 +9,10 @@ else
  key=$1
 fi
 
-wallet=$(echo $PWD | pryzmd keys show $key --output json | jq -r .address)
-balance=$(pryzmd query bank balances $wallet -o json | jq -r .balances[].amount | awk '{print $1/1000000}')
-echo "Balance: $balance pryzm"
+wallet=$(echo $PWD | pryzmd keys show $key -a)
+balance1=$(pryzmd query bank balances $wallet -o json 2>/dev/null \
+      | jq -r '.balances[] | select(.denom=="upryzm")' | jq -r .amount | awk '{print $1/1000000}')
+echo "Balance: $balance1 pryzm"
 
 if [ -z $2 ]
 then
