a2ed330886fabe0fa7f9f0ed681266e435ddf2ba
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 21:00:53 +0100

    Update delegate.sh

--- delegate.sh
+++ delegate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-source ~/scripts/pryzm/config/env
+
+FOLDER=$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')
+source ~/scripts/$(echo $(cd -- $(dirname -- "${BASH_SOURCE[0]}") && pwd) | awk -F/ '{print $NF}')/config/env
 
 if [ -z $1 ]
 then
@@ -9,14 +11,14 @@ else
  key=$1
 fi
 
-wallet=$(echo $PWD | pryzmd keys show $key -a)
-balance1=$(pryzmd query bank balances $wallet -o json 2>/dev/null \
+wallet=$(echo $PWD | $BINARY keys show $key -a)
+balance=$($BINARY query bank balances $wallet -o json 2>/dev/null \
       | jq -r '.balances[] | select(.denom=="upryzm")' | jq -r .amount | awk '{print $1/1000000}')
-echo "Balance: $balance1 pryzm"
+echo "Balance: $balance pryzm"
 
 if [ -z $2 ]
 then
- def_valoper=$(echo $PWD | pryzmd keys show $key -a --bech val)
+ def_valoper=$(echo $PWD | $BINARY keys show $key -a --bech val)
  read -p "To valoper (default $def_valoper) ? " valoper
  if [ -z $valoper ]; then valoper=$def_valoper; fi
 else
@@ -32,5 +34,5 @@ fi
 
 amount=$(( $amount * 1000000 ))upryzm
 
-echo $PWD | pryzmd tx staking delegate $valoper $amount --from $key \
- --chain-id indigo-1 --gas-prices 0.1upryzm --gas-adjustment 1.5 --gas auto -y
+echo $PWD | $BINARY tx staking delegate $valoper $amount --from $key \
+ --chain-id $NETWORK --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJ --gas auto -y
