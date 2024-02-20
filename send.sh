76e92c75fe06b7bca83a00ea399d311155557725
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 20:43:47 +0100

    Update send.sh

--- send.sh
+++ send.sh
@@ -2,23 +2,10 @@
 
 source ~/scripts/pryzm/config/env
 
-if [ -z $1 ]
-then
- read -p "From key or wallet ? " from
-else
- from=$1
-fi
-
-if [ -z $2 ]
-then
- read -p "To wallet ? " to
-else
- to=$2
-fi
-
+[ -z $1 ] && from=$1 || read -p "From key or wallet ? " from
+[ -z $2 ] && to=$2 ||  read -p "To wallet ? " to
 echo "Denoms: upryzm   factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim"
-
-[ -z $3 ] && read -p "Amount (incl. denom) ? " amount || amount=$3
+[ -z $3 ] && amount=$3 || read -p "Amount (incl. denom) ? " amount
 
 echo $PWD | pryzmd tx bank send $from $to $amount \
    --gas-prices 0.1upryzm --gas-adjustment 1.5 --gas auto -y
