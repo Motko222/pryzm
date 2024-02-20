e092ec003b4460fbf9e6d0cfac831aab4ac58518
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-02-20 20:49:31 +0100

    Update send.sh

--- send.sh
+++ send.sh
@@ -2,7 +2,7 @@
 
 source ~/scripts/pryzm/config/env
 
-[ -z $1 ] && from=$1 || read -p "From key or wallet ? " from
+[ -z $1 ] && read -p "From key or wallet ? " from || from=$1
 
 wallet=$($BINARY keys show $from -a)
 balance1=$($BINARY query bank balances $wallet -o json 2>/dev/null \
@@ -11,8 +11,8 @@ balance2=$($BINARY query bank balances $wallet -o json 2>/dev/null \
       | jq -r '.balances[] | select(.denom=="factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim")' | jq -r .amount )
 echo Balance $((balance1))upryzm $((balance2))factory/pryzm15k9s9p0ar0cx27nayrgk6vmhyec3lj7vkry7rx/uusdsim
 
-[ -z $2 ] && to=$2 ||  read -p "To wallet ? " to
-[ -z $3 ] && amount=$3 || read -p "Amount (incl. denom) ? " amount
+[ -z $2 ] && read -p "To wallet ? " to || to=$2
+[ -z $3 ] && read -p "Amount (incl. denom) ? " amount || amount=$3
 
 echo $PWD | pryzmd tx bank send $from $to $amount \
    --gas-prices $GAS_PRICE --gas-adjustment $GAS_ADJ --gas auto -y
