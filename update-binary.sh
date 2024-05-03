3b99a74bcbf57c9a5a0f7dd6332a0d64a6e7b304
Motko222 <77466238+Motko222@users.noreply.github.com>
2024-05-03 15:18:10 +0200

    Update update-binary.sh

--- update-binary.sh
+++ update-binary.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-read -p "Sure ? " sure
-if [ $sure -ne "y" -a $sure -ne "Y" ]; then exit 1; fi
-
 read -p "URL? " url
 
 cd /usr/local/bin
 rm pryzmd
-wget $url -o pryzmd
+wget $url -O pryzmd
 chmod +x pryzmd
 
 echo "New binary installed."
